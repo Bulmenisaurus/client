bb3e24a7ed8e37c29bb92719566793aa0c6ac09a
alexey <alexeybulmeni@gmail.com>
2021-07-20 14:45:03 -0700

    add `onbeforeunload` listener
    this prevents accidentally reloading the page and having to wait for minutes to log back in

--- src/Frontend/EntryPoints/index.tsx
+++ src/Frontend/EntryPoints/index.tsx
@@ -8,4 +8,12 @@ import '../Styles/style.css';
 import '../Styles/icomoon/style.css';
 import App from '../Pages/App';
 
+// source: https://developer.mozilla.org/en-US/docs/Web/API/WindowEventHandlers/onbeforeunload
+window.addEventListener('beforeunload', (e) => {
+  // Cancel the event
+  e.preventDefault();
+  // Chrome requires returnValue to be set
+  e.returnValue = '';
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
